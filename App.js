db42346cca70ee95d9bef881600ec58260a26152
EDUARDO BENITEZ <eduardobenitez@MacBook-Pro-de-EDUARDO.local>
2018-10-15 20:48:01 -0500

    react-native with native base

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { WebView } from 'react-native';
-
-
-export default class App extends React.Component {
-  
+import React, {Component} from 'react';
+import {Text, View  } from 'native-base';
+//import {StyleProviderThemed} from './StyleProviderThemed'
+export default class App extends Component {
   render() {
     return (
-      <WebView
-        source={{uri: 'https://mxasturias01.firebaseapp.com'}}
-        style={{marginTop: 20}}
-      />
+        <View>
+            <Text>
+              I have changed the text color.
+            </Text>
+            <Text>
+              I have changed the text color.
+            </Text>
+            <Text>
+              I have changed the text color.
+            </Text>
+        </View>
+    
     );
   }
 }
